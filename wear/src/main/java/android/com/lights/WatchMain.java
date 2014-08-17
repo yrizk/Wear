5ca7deebb8d0a37da60979e06d424de227063f64
why_rizk <rizk.yohanna@gmail.com>
2014-08-17 09:30:48 -0400

    got a red circle up. horray

--- wear/src/main/java/android/com/lights/WatchMain.java
+++ wear/src/main/java/android/com/lights/WatchMain.java
@@ -2,17 +2,25 @@ package android.com.lights;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class WatchMain  extends Activity {
 
-    private TextView mTextView;
+    private ViewGroup viewGroup;
     private final String TAG = WatchMain.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_main);
+
     }
+
+    public void startAnimations(View v){
+
+    }
+
+
 
 
 }
